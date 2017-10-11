896a726763a88712078ab8140629758934c24c46
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-11 08:12:33 +0100

    testing

--- PHAS3459/src/module2/Test.java
+++ PHAS3459/src/module2/Test.java
@@ -1,10 +1,14 @@
 package module2;
 
 public class Test {
+	
+	public void VectorTest(double a, double b,double c) {
+	Vector u = new ThreeVector(a,b,c); //Creating a threevector
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
