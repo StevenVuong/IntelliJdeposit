136b325b64b08f9948a9b776051693a9e106116c
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-11 16:08:06 +0100

    Completed all of static variables, have done non static for scalar product

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -1,6 +1,6 @@
 package module2;
 
-public class ThreeVector{
+public class ThreeVector {
 
 	private double x;
 	private double y;
@@ -27,27 +27,42 @@ public class ThreeVector{
 		return c;
 	}
 
-	 public static double scalarProduct(ThreeVector vector1,ThreeVector vector2) {
-		 return ((vector1.x * vector2.x) + (vector1.y * vector2.y) + (vector1.z * vector2.z));
-	 }
+	public static double scalarProduct(ThreeVector vector1, ThreeVector vector2) {
+		return ((vector1.x * vector2.x) + (vector1.y * vector2.y) + (vector1.z * vector2.z));
+	}
 
-	 public static ThreeVector vectorProduct(ThreeVector vector1, ThreeVector vector2) {
-		 double vpi = ((vector1.y*vector2.z)-(vector1.z*vector2.y));
-		 double vpj = ((vector1.x*vector2.z)-(vector1.z*vector2.x));
-		 double vpk = ((vector1.x*vector2.y)-(vector1.y*vector2.x));
-		 ThreeVector vp = new ThreeVector(vpi, vpj, vpk);
-		 return vp;
-	 }
-	 
-	 public double scalarProduct(Th...) {
-		 this.scalarProduct(v1,v2 ..getClass().;)
-	 }
+	public static ThreeVector vectorProduct(ThreeVector vector1, ThreeVector vector2) {
+		double vpi = ((vector1.y * vector2.z) - (vector1.z * vector2.y));
+		double vpj = ((vector1.x * vector2.z) - (vector1.z * vector2.x));
+		double vpk = ((vector1.x * vector2.y) - (vector1.y * vector2.x));
+		ThreeVector vp = new ThreeVector(vpi, vpj, vpk);
+		return vp;
+	}
+
+	public static ThreeVector add(ThreeVector vector1, ThreeVector vector2) {
+		ThreeVector vectorAdd = new ThreeVector((vector1.x + vector2.x), (vector1.y + vector2.y),
+				(vector1.z + vector2.z));
+		return vectorAdd;
+	}
+
+	public static double angle(ThreeVector vector1, ThreeVector vector2) {
+		double dot = scalarProduct(vector1, vector2);
+		double mag1 = vector1.magnitude();
+		double mag2 = vector2.magnitude();
+		return Math.acos(dot / (mag1 * mag2)); // calculates angle
+
+	}
+
+	public double scalarProduct(ThreeVector vector) {
+		return ThreeVector.scalarProduct(this, vector);
+	}
+
 	public static void main(String[] args) {
 		ThreeVector testVector = new ThreeVector(1.0, 2.0, 3.0);
 		ThreeVector testVector2 = new ThreeVector(1.0, 2.0, 3.0);
 		ThreeVector.scalarProduct(testVector, testVector2);
-		testVector.scalarProduct(); //non static calling. Belongs to object
-//		System.out.println(v);
+		testVector.scalarProduct(testVector2); // non static calling. Belongs to object
+		// System.out.println(v);
 	}
 
 }
