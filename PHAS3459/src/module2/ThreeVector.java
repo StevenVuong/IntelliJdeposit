a10be0746e2342d574a00024708a27780a5daabf
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-11 13:57:26 +0100

    Have done vectorProduct

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -1,11 +1,11 @@
 package module2;
 
-public class ThreeVector {
+public class ThreeVector{
 
 	private double x;
 	private double y;
 	private double z;
-	
+
 	public ThreeVector(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
@@ -13,17 +13,41 @@ public class ThreeVector {
 	}
 
 	public double magnitude() {
-		double b = Math.sqrt(((x * x) + (y * y) + (z * z)));
-		return b;
+		return Math.sqrt(((x * x) + (y * y) + (z * z)));
 	}
 
 	public ThreeVector unitVector() {
-		double magntitude = magnitude();
-		return new ThreeVector(x/magntitude, y/magntitude, z/magntitude);
+		double mag = magnitude();
+		return new ThreeVector(x / mag, y / mag, z / mag);
 	}
 
+	@Override
+	public String toString() {
+		String c = "the x value is: " + x + ", the y value is: " + y + ", and the z value is: " + z;
+		return c;
+	}
+
+	 public static double scalarProduct(ThreeVector vector1,ThreeVector vector2) {
+		 return ((vector1.x * vector2.x) + (vector1.y * vector2.y) + (vector1.z * vector2.z));
+	 }
+
+	 public static ThreeVector vectorProduct(ThreeVector vector1, ThreeVector vector2) {
+		 double vpi = ((vector1.y*vector2.z)-(vector1.z*vector2.y));
+		 double vpj = ((vector1.x*vector2.z)-(vector1.z*vector2.x));
+		 double vpk = ((vector1.x*vector2.y)-(vector1.y*vector2.x));
+		 ThreeVector vp = new ThreeVector(vpi, vpj, vpk);
+		 return vp;
+	 }
+	 
+	 public double scalarProduct(Th...) {
+		 this.scalarProduct(v1,v2 ..getClass().;)
+	 }
 	public static void main(String[] args) {
-		ThreeVector threevector1 = new ThreeVector(1.0, 2.0, 3.0);
+		ThreeVector testVector = new ThreeVector(1.0, 2.0, 3.0);
+		ThreeVector testVector2 = new ThreeVector(1.0, 2.0, 3.0);
+		ThreeVector.scalarProduct(testVector, testVector2);
+		testVector.scalarProduct(); //non static calling. Belongs to object
+//		System.out.println(v);
 	}
 
 }
