56dca45b064a58f2e535b3716ef06c1df7906b1b
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-11 08:58:18 +0100

    Got the magnitude down, working on three vector

--- PHAS3459/src/module2/Test.java
+++ PHAS3459/src/module2/Test.java
@@ -1,10 +1,16 @@
 package module2;
-
 public class Test {
 	
-	public void VectorTest(double a, double b,double c) {
-	Vector u = new ThreeVector(a,b,c); //Creating a threevector
+public double magnitude(double x1,double y1,double z1) {
+	 double b = Math.sqrt(((x1*x1)+(y1*y1)+(z1*z1)));
+	 return b;
 	}
+	
+public ThreeVector(double a, double b,double c) {
+		
+	return c;
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
