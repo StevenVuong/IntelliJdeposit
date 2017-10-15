65bb55a597c3f45bad84bbdd9d3189654845b0fe
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-15 18:19:53 +0100

    Added many comments

--- PHAS3459/src/module2/Complex.java
+++ PHAS3459/src/module2/Complex.java
@@ -1,16 +1,17 @@
 package module2;
 
 public class Complex {
-
+	
+	//Creates real and imaginary variables for this class
 	private double real;
 	private double imagine;
 
-	public Complex(double re, double im) {
+	public Complex(double re, double im) { //Creates a constructor in order to set complex numbers
 		this.real = re;
 		this.imagine = im;
 	}
 
-	//Creating static Complex Numbers 
+	//Creating static Complex Numbers (Note that they are also final, so that they are set and unchangeable)
 	public static final Complex ONE = new Complex(1,0);
 	public static final Complex ZERO = new Complex(0,0);
 	public static final Complex I = new Complex(0,1);
@@ -36,12 +37,12 @@ public class Complex {
 		return a;
 	}
 
-	public Complex normalised() {
-		double mod = modulus();
+	public Complex normalised() { //returns the normalised complex number
+		double mod = modulus(); //calculates the modulus and assigns it to a 'mod' variable to divide by
 		return new Complex((real / mod), (imagine / mod));
 	}
 
-	public boolean equals(Complex c) {
+	public boolean equals(Complex c) { //creates a boolean which returns 'true' if and only if the complex argument is equal to the current real and imaginary number
 		Complex test = new Complex(real, imagine);
 		if (c == test) {
 			return true;
@@ -49,32 +50,32 @@ public class Complex {
 			return false;
 	}
 
-	public String toString() {
+	public String toString() { //method returning complex number in string form
 		return ("re=" + real + ", im=" + imagine+ "i");
 	}
 
-	public Complex setFromModulusAngle(double mag, double ang) {
+	public Complex setFromModulusAngle(double mag, double ang) { //method returning complex number from modulus and angle
 			double x1 = mag*Math.cos(ang);
 			double y1 = mag*Math.sin(ang);
 			Complex a = new Complex(x1,y1);
 			return a;
 	}
 
-	public static Complex add(Complex a, Complex b) {
+	public static Complex add(Complex a, Complex b) { //method adding two complex numbers together
 		return new Complex((a.real + b.real),(a.imagine+b.imagine));
 	}
 
-	public static Complex subtract(Complex a, Complex b) {
+	public static Complex subtract(Complex a, Complex b) { //method subtracting one complex number from another
 		return new Complex((a.real - b.real),(a.imagine - b.imagine));
 	}
 	
-	public static Complex multiply(Complex a, Complex b) {
+	public static Complex multiply(Complex a, Complex b) { //method multiplying two complex numbers together
 		double x1 = ((a.real*b.real)-(a.imagine*b.imagine));
 		double y1 = ((a.real*b.imagine)+(a.imagine*b.real));
 		return new Complex (x1,y1);
 	}
 	
-	public static Complex divide(Complex a, Complex b) {
+	public static Complex divide(Complex a, Complex b) { //method dividing one complex number by another
 		double real = ((a.real*b.real)+(a.imagine*b.imagine));
 		double imag = ((a.imagine*b.real)-(a.real*b.imagine));
 		double mag = ((b.real*b.real)+(b.imagine*b.imagine));
--- PHAS3459/src/module2/TestThreeVector.java
+++ PHAS3459/src/module2/TestThreeVector.java
@@ -3,7 +3,7 @@ package module2;
 public class TestThreeVector {
 
 	public static void main(String[] args) {
-		// Creating & Printing vectors
+		// Creating & Printing vectors, v1,v2 and v3
 		ThreeVector v1 = new ThreeVector(3, 5, 2);
 		ThreeVector v2 = new ThreeVector(2, 4, 1);
 		ThreeVector v3 = new ThreeVector(0, 0, 0);
@@ -12,19 +12,21 @@ public class TestThreeVector {
 		System.out.println("v3: " + v3);
 
 		System.out.println("");
-		// static scalar & vector product
+		
+		// Calculating static scalar & vector product and assigning these to variables
 		double scalarStaticv1v2 = ThreeVector.scalarProduct(v1, v2);
 		double scalarStaticv1v3 = ThreeVector.scalarProduct(v1, v3);
 		ThreeVector vectorStaticv1v2 = ThreeVector.vectorProduct(v1, v2);
 		ThreeVector vectorStaticv1v3 = ThreeVector.vectorProduct(v1, v3);
 
-		// non-static scalar & vector product
+		// Calculating non-static scalar & vector product & assigning these to variables
 		double scalarNonStaticv1v2 = v1.scalarProduct(v2);
 		double scalarNonStaticv1v3 = v1.scalarProduct(v3);
 		ThreeVector vectorNonStaticv1v2 = v1.vectorProduct(v2);
 		ThreeVector vectorNonStaticv1v3 = v1.vectorProduct(v3);
 
 		System.out.println("");
+		
 		// Printing both static & non-static scalar/vector products
 		System.out.println("Static scalar product of v1 & v2 is: " + scalarStaticv1v2
 				+ " and the non-static equivalent is: " + scalarNonStaticv1v2);
@@ -46,9 +48,10 @@ public class TestThreeVector {
 		System.out.println("");
 		// Printing both static & non-static angles
 		System.out.println("Static angle between v1 & v2 is: " + angleStaticv1v2 + " and the non-static equivalent is: "
-				+ angleNonStaticv1v2); // Answers in radians?
+				+ angleNonStaticv1v2+" radians"); 
 		System.out.println("Static angle between v1 & v3 is: " + angleStaticv1v3 + " and the non-static equivalent is: "
-				+ angleNonStaticv1v3); // returns NaN?
+				+ angleNonStaticv1v3+" radians, this answer is NaN as one of the vectors is (0,0,0)"); 
+		System.out.println("Both the static and non-static return the same value for all calculations");
 
 		System.out.println("");
 		// System.out.println(v);
@@ -56,7 +59,7 @@ public class TestThreeVector {
 				"When I try to run 'System.out.println(v)' without a toString(), it states v cannot be resolved to a variable.");
 		System.out.println(
 				"This implies that the code does not acknowledge v as a string until defined so with tostring().");
-		System.out.println("This is because v is not defined as a vector.");
+		System.out.println("This is because v is not defined as a vector. This would be solved if I were to set it to v1 instead of v");
 
 	}
 
