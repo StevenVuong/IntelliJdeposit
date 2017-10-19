a66849453393e70387e8e2fe72c174de778c5522
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-19 20:12:52 +0100

    Ready to submit

--- PHAS3459/src/module3/Alphabet.java
+++ PHAS3459/src/module3/Alphabet.java
@@ -1,5 +1,6 @@
 package module3;
 
+//importing relevant modules
 import java.util.Random;
 import java.lang.Character;
 import java.lang.Integer;
@@ -7,17 +8,17 @@ import java.lang.StringBuilder;
 
 public class Alphabet {
 
-	private static Random rnd = new Random();
+	private static Random rnd = new Random(); // Creating random object
 
-	public static char randomCharacter() {
+	public static char randomCharacter() { // Creating static method to generate numbers 0-127
 		int randomNumber1 = rnd.nextInt(128); // 0 to 127
 		char charRandom = (char) randomNumber1; // casts the random number into a character
 		return charRandom;
 	}
 
 	public static StringBuilder randomString = new StringBuilder(); // initialises a string with StringBuilder
-	public static int randomSums = 0;
-	public static int randomExceptions = 0;
+	public static int randomSums = 0; //initialises summation of random digits
+	public static int randomExceptions = 0; //initialises counter for number of exceptions
 
 	public static void main(String[] args) throws Exception {
 
@@ -25,17 +26,17 @@ public class Alphabet {
 		while (counter < 250) { // creates a while loop, setting the condition to run 250 iterations if counter
 								// is increased by 1 each time
 			char randomChar = randomCharacter(); // creates a random character
-			boolean randomCharBoolean = Character.isLetterOrDigit(randomChar);
+			boolean randomCharBoolean = Character.isLetterOrDigit(randomChar); // sets the boolean for a random
+																				// character, if it is a letter or digit
 			if (randomCharBoolean == true) { // creates condition for 'if' loop
 				randomString.append(randomChar); // adds the letter/digit to the string
 
 				try {
-					String randomString = Character.toString(randomChar); //converts character to string
+					String randomString = Character.toString(randomChar); // converts character to string
 					Integer randomLetterOrDigit = Integer.parseInt(randomString); // parse integer
-
-					randomSums += randomLetterOrDigit; //adds the integer onto the current summation
+					randomSums += randomLetterOrDigit; // adds the integer onto the current summation
 				} catch (Exception e) {
-					randomExceptions ++; //adds one to count the number of exceptions
+					randomExceptions++; // adds one to count the number of exceptions
 				}
 			}
 			counter++;
@@ -43,9 +44,8 @@ public class Alphabet {
 		System.out.println("the StringBuilder string is: ");
 		System.out.println(randomString);
 		System.out.println("");
-		System.out.println("The sum of numbers added together are: "+randomSums);
-		System.out.println("The number of exceptions thrown by Integer.parseInt() is: "+randomExceptions);
-
+		System.out.println("The sum of numbers added together are: " + randomSums);
+		System.out.println("The number of exceptions thrown by Integer.parseInt() is: " + randomExceptions);
 
 	}
 
