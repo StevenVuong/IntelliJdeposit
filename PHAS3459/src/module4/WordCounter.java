760a9ccb5c7e04f5bb0dce1c39f3ff6fced97110
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-23 17:17:53 +0100

    Add comments, check word counter argument

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -1,18 +1,25 @@
 package module4;
 
 import java.io.*;
+import java.net.URL;
 import java.util.Scanner;
-
-import module2.Complex;
 
 public class WordCounter {
 
-	public static BufferedReader brFromURL(String urlName) throws FileNotFoundException  {
-		return new BufferedReader(new FileReader(urlName));
+	public static BufferedReader brFromURL(String urlName) throws FileNotFoundException, IOException {
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader b = new BufferedReader(isr);
+		return b;
 	}
 
-	public static BufferedReader brFromFile(String fileName) throws FileNotFoundException {
-		return new BufferedReader(new FileReader(fileName));
+	public static BufferedReader brFromFile(String fileName) throws IOException {
+		File u = new File(fileName);
+		FileReader isr = new FileReader(u);
+		BufferedReader b = new BufferedReader(isr);
+		return b;
+		// return new BufferedReader(new FileReader(fileName));
 	}
 
 	public static int countWordsInResource(BufferedReader dataAsBR) {
@@ -26,19 +33,24 @@ public class WordCounter {
 		return (int) count;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		try {
-			BufferedReader brFromURL = WordCounter.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			BufferedReader brFromURL = WordCounter
+					.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			System.out.println("The returned buffer reader object is" + brFromURL);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		try {
-			BufferedReader brFromFile = WordCounter.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			BufferedReader brFromFile = WordCounter.brFromFile("C:\\Users\\Steven Vuong\\Downloads\\module4_text.txt");
+			System.out.println("The returned buffer reader object is: " + brFromFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		try {
-			int brFromFile = WordCounter.countWordsInResource(brFromFile("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"));
+			int brFromFile = WordCounter.countWordsInResource(
+					brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"));
+			System.out.println("The number of words in this file is: " + brFromFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
