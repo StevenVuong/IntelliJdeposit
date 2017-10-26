c248a889a2ceaf8d64f52f5dfdc3e3f5ea523954
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-26 05:51:44 +0100

    Need to solve empty keyboard input

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -2,6 +2,8 @@ package module4;
 
 import java.io.*;
 
+import module1.AlgorithmControl;
+
 public class NumericalReader {
 
 	private double minValue;
@@ -56,15 +58,27 @@ public class NumericalReader {
 		if ((line.trim().isEmpty()) || line.startsWith("//")) { // check if line is empty or starts with comment line
 			return; // return is like a way of doing nothing
 		}
-		String numLine = line.replaceAll("[^0-9]+", " "); // This returns the numbers
-		String[] arrayOfStringBasedNumbers = numLine.trim().split(" "); // which returns number array
-		System.out.println("creates an array of all the numbers" + arrayOfStringBasedNumbers);
-		System.out.println("To Test");
-
 		try {
+
+			String numLine = line.replaceAll("[^0-9]+", " "); // This returns the numbers only in a line
+			// Insert numbers read in
+			double a = Double.parseDouble(numLine);
+			nValues++;
+			sumOfValues = sumOfValues + a;
+			if (a < minValue) {
+				minValue = a;
+			}
+			if (a > maxValue) {
+				maxValue = a;
+			}
+
+			String[] arrayOfStringBasedNumbers = numLine.trim().split(" "); // which returns number array
+			System.out.println("creates an array of all the numbers" + arrayOfStringBasedNumbers);
+			System.out.println("To Test");
+
 			BufferedWriter b = new BufferedWriter(new FileWriter(line));
 			PrintWriter pw = new PrintWriter(b);
-			pw.println(arrayOfStringBasedNumbers);
+			pw.println(arrayOfStringBasedNumbers); // prints array of numbers to file
 			pw.close();
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
@@ -72,15 +86,38 @@ public class NumericalReader {
 
 	}
 
-	public static BufferedReader wc;
-
-	public static void main(String[] args) {
-
-		NumericalReader nr = new NumericalReader();
-
-		String s = NumericalReader.getStringFromKeyboard(); // test
-		System.out.println("s is: " + s);
-
+	void analysisEnd() {
+		System.out.println("the maximum value is: " + maxValue);
+		System.out.println("the minimum value is: " + minValue);
+		System.out.println("the total number of values read is: " + nValues);
+		double average = sumOfValues / nValues;
+		System.out.println("the average value is: " + average);
 	}
+
+	public static void main(String[] args) throws Exception {
+		
+		String line = "";
+		// String empty = new String("");
+		
+		String saveDir = NumericalReader.getStringFromKeyboard(); // allows user to specify the save Directory
+		while (saveDir.trim().isEmpty()) { // check if line is empty
+			String saveDir = new String(System.getProperty("user.home"));
+		}
+
+	}System.out.println("save Directory is: "+saveDir);
+
+	String saveFile = (saveDir + File.separator + dataFile); // uses File.separator so user doesn't have to specify
+																// trailing slash and the end of each directory name
+	NumericalReader nr = new NumericalReader();
+
+	BufferedReader reader = nr.brFromURL("http://foo/bar/");
+	String line = "";
+
+	nr.analysisStart(saveFile); // initialize minValue etc.
+
+	String a = new String("5 0 88 9");
+	v twofoureight1;twofoureight1=new analyseData(a);b=new analyseData(a);
+
+}
 
 }
