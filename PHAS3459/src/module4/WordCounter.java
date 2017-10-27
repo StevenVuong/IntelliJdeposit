989b6d6a1a9857acfbcae4304cdba70815ad210a
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-10-27 11:15:32 +0100

    added comments, fully functioning

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -5,53 +5,71 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class WordCounter {
-	
-	public static BufferedReader brFromURL(String urlName) throws FileNotFoundException, IOException { //creates static method for returning buffered reader from URL string with exception for error
-		URL u = new URL(urlName); //creates URL object
-		InputStream is = u.openStream(); //effectively searches for the url
+
+	private static String urlText = ("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt"); // creates
+																												// string
+																												// of
+																												// text
+																												// file
+																												// for
+																												// this
+																												// class
+	private static String fileDirectory = ("C:\\Users\\Steven Vuong\\Downloads\\module4_text.txt"); // creates string of
+																									// fileDirectory for
+																									// this class
+
+	public static BufferedReader brFromURL(String urlName) throws FileNotFoundException, IOException { // creates static
+																										// method for
+																										// returning
+																										// buffered
+																										// reader from
+																										// URL string
+																										// with
+																										// exception for
+																										// error
+		URL u = new URL(urlName); // creates URL object
+		InputStream is = u.openStream(); // effectively searches for the url
 		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader b = new BufferedReader(isr); //sets the buffered reader as the inputStreamReader
+		BufferedReader b = new BufferedReader(isr); // sets the buffered reader as the inputStreamReader
 		return b;
 	}
 
-	public static BufferedReader brFromFile(String fileName) throws IOException { //creates static method for returning buffered reader from fileName
-		File u = new File(fileName); //creates new file
-		FileReader isr = new FileReader(u); //searches for file
-		BufferedReader b = new BufferedReader(isr); //creates buffered reader object
+	public static BufferedReader brFromFile(String fileName) throws IOException { // creates static method for returning
+																					// buffered reader from fileName
+		File u = new File(fileName); // creates new file
+		FileReader isr = new FileReader(u); // searches for file
+		BufferedReader b = new BufferedReader(isr); // creates buffered reader object
 		return b;
 
 	}
 
-	public static int countWordsInResource(BufferedReader dataAsBR) {
-		int count = 0;
-		try (Scanner s = new Scanner(dataAsBR);) {
-			while (s.hasNext()) {
+	public static int countWordsInResource(BufferedReader dataAsBR) { // creates static method for counting words in a
+																		// file
+		int count = 0; // initialises counter
+		try (Scanner s = new Scanner(dataAsBR);) { // sets scanner, breaks apart each string of characters
+			while (s.hasNext()) { // sets condition for loop
 				s.next();
-				count++;
+				count++; // increases counter by 1
 			}
 		}
 		return (int) count;
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { // main method with 'throws Exception'
 		try {
-			BufferedReader brFromURL = WordCounter
-					.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
+			BufferedReader brFromURL = WordCounter.brFromURL(urlText); // gets buffered reader using brFromURL method
 			System.out.println("The returned buffer reader object is" + brFromURL);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		try {
-			BufferedReader brFromFile = WordCounter.brFromFile("C:\\Users\\Steven Vuong\\Downloads\\module4_text.txt");
+			BufferedReader brFromFile = WordCounter.brFromFile(fileDirectory); //gets buffered reader from file using brFromFile method
 			System.out.println("The returned buffer reader object is: " + brFromFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		try {
-			int brFromFile = WordCounter.countWordsInResource(
-					brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt")); // what file in
-																											// particular?
-
+			int brFromFile = WordCounter.countWordsInResource(brFromURL(urlText)); //counts words in URL
 			System.out.println("The number of words in this file is: " + brFromFile);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
