0788229ec700a89b5afd525a37ae335bac88f410
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-01 17:27:54 +0000

    Completed the challenge?

---
+++ PHAS3459/src/module5/TreatWell.java
@@ -0,0 +1,18 @@
+package module5;
+
+public class TreatWell {
+public static void main(String[] args) {
+	int n = 4; //set dimensions
+	for (int i=1; i<=n;i++) {
+		for (int j=1; j<n; j++) {
+			if(i==1||j==1) {
+				System.out.println("-");//prints horizontal lines
+			}
+			else if (j==1||j==n) {
+				System.out.println("|");//prints vertical lines
+			}
+			else System.out.println(" "); 
+		}
+	}
+}
+}
