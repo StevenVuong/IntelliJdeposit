7ff1643e5312787b4baf4915ed937dcea4280726
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-04 09:46:43 +0000

    created goodness of fit method. needs finishing

--- PHAS3459/src/module5/DataAnalysis.java
+++ PHAS3459/src/module5/DataAnalysis.java
@@ -3,16 +3,27 @@ package module5;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class DataAnalysis {
 
-	// creates static method for returning buffered reader from URL string
+	// creates static method for returning arraylist from URL string
 	public static ArrayList<DataPoint> dataFromURL(String urlName) throws FileNotFoundException, IOException {
 		URL u = new URL(urlName); // creates URL object
-		InputStream is = u.openStream(); // effectively searches for the url
+		InputStream is = u.openStream(); // searches for the url
 		InputStreamReader isr = new InputStreamReader(is);
-		ArrayList b = new ArrayList(isr); // sets the buffered reader as the inputStreamReader
+		BufferedReader b = new BufferedReader(isr); // sets the buffered reader as the inputStreamReader
+		
+		ArrayList<DataPoint> point = new ArrayList<DataPoint>();
+		String empty = new String(""); //initialises String
+		while ((empty = b.readLine()) != null) {
+			point.add(DataPoint.parseline(empty)); // object to string with parseline then adds to ArrayList
+		}
+		return point;
+	}
+	
+	public static double goodnessOfFit(Theory t, ArrayList<DataPoint> dataPoint) {
+		
+		
 		return null;
 	}
 }
