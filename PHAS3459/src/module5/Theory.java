30a183c8d818c690089f51c64e5ab6fa57fbf625
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-04 08:51:09 +0000

    yet to add the comparison to th

--- PHAS3459/src/module5/Theory.java
+++ PHAS3459/src/module5/Theory.java
@@ -1,5 +1,26 @@
 package module5;
 
+import java.util.ArrayList;
+
 public class Theory {
+
+	//set member variables
+	private double n;
+	private double x;
+	private double y;
+
+	//constructor for variables
+	public Theory(double n) {
+		
+		this.n = n;
+	
+	}
+
+	public double y(double x) {
+		
+		double Y = Math.pow(x, this.n);
+		return Y;
+	
+	}
 
 }
