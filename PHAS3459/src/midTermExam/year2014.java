f52c95cabdb13b94a32837490e7b7e8c9a94fba0
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-10 15:21:44 +0000

    completely works, uses hashmap to sort out the methods

--- PHAS3459/src/midTermExam/year2014.java
+++ PHAS3459/src/midTermExam/year2014.java
@@ -60,7 +60,7 @@ public class year2014 {
 	public static void main(String[] args) {
 		try {
 			ArrayList<Planet> allPlanets = dataFromURLPlanet("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2014-15/exoplanets.txt");
-			System.out.println("there are  " + allPlanets.size() + " planets in total in this list");
+			System.out.println("there are  " + allPlanets.size() + " exoplanets discovered in total in this list");
 			
 			//initialising values to hold planet with smallest distance
 			Planet planetSmallestDistance = null;
@@ -74,9 +74,56 @@ public class year2014 {
 					planetSmallestDistanceDetails = p.getPlanetDetails();
 				}
 			}
+			
 			System.out.println("Smallest Planet Distance to Earth Details: " + planetSmallestDistanceDetails);
 			
+			HashMap<String, ArrayList<Planet>> planetsByMethod = new HashMap<String, ArrayList<Planet>>(); // Initialise HashMap to hold method names as keys and an ArrayList of planets as the values.
+			
+			// sort planets by method
+			for (Planet p : allPlanets) { //loops over all planets
+				if (planetsByMethod.get(p.method) == null) { // If the method has not yet been put into the HashMap, create a new ArrayList containing the current planet and put it into the HashMap.
+					ArrayList<Planet> planetArray = new ArrayList<Planet>(); 
+					planetArray.add(p);
+					planetsByMethod.put(p.method, planetArray); 
+				}
+				else { //the method is already registered as a hashmap, scans hashmap for key, add current planet to arraylist and update hashmap
+					ArrayList<Planet> currentMethod = planetsByMethod.get(p.method);
+					currentMethod.add(p);
+					planetsByMethod.put(p.method, currentMethod); //associates key to value
+				}
+			}
+			
+			for (String m: planetsByMethod.keySet()) { // Do the following for each method: loops through each method and gets key, m
+				
+				//initialising variables
+				int earliestYear = 3000;
+				double lightestMass = 3000;
+				String lightestMassDetails = null;
+				
+				ArrayList<Planet> methodPlanet = planetsByMethod.get(m); //retrieve array list for method currently being considered
+				
+				for (Planet pMethod : methodPlanet) {
+					
+					if (pMethod.year < earliestYear) {
+						earliestYear = pMethod.year;
+					}
+					
+					if (pMethod.mass < lightestMass) {
+						lightestMass = pMethod.mass;
+						lightestMassDetails = pMethod.getPlanetDetails();
+					}
+					
+				}
+				System.out.println("");
+				System.out.println("Method of Discovery: " + m);
+				System.out.println("The number of explanets discovered is: " + methodPlanet.size());
+				System.out.println("the earliest year an exoplanet was discovered with this was: " + earliestYear);
+				System.out.println("the lightest exoplanet has details: " + lightestMassDetails);
+				
+			}
 		}
+		
+		
 		catch (Exception e) {
 			System.out.println(e);
 		}
