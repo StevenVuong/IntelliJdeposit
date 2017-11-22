4658e7d3973b65040d1c9da4acb4eef9f527bcd1
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-22 21:31:15 +0000

    completed abstract class as member of theory. Ready to submit

---
+++ PHAS3459/src/module6/PowerLawTheory.java
@@ -0,0 +1,26 @@
+package module6;
+
+public abstract class PowerLawTheory implements Theory {
+	//class holding mathematical relation between x and y value (powerOf relationship)
+	
+	//define member variables (n is the power)
+	double n;
+	double x;
+	double y;
+	
+	//constructor for PowerLawTheory object, setting parameters of function
+	public PowerLawTheory(double n) {
+		this.n = n;
+	}
+	
+	//returns Y = x^n
+	public double y(double x) {
+		return Math.pow(x, n);
+	}
+	
+	//toString method stating the function
+	public String toString() {
+		return ("x^"+n);
+	}
+	
+}
