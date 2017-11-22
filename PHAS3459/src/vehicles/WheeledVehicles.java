6ca994a0cf5fc68847e8a592f84f1705394dde3e
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-22 21:56:49 +0000

    hari would not approve

--- PHAS3459/src/vehicles/WheeledVehicles.java
+++ PHAS3459/src/vehicles/WheeledVehicles.java
@@ -2,13 +2,14 @@ package vehicles;
 
 public abstract class WheeledVehicles implements Vehicles {
 	private int wheels;
+
 	public WheeledVehicles(int wheels) {
-		this.wheels=wheels;
+		this.wheels = wheels;
 	}
-	
+
 	public int getWheels() {
 		return wheels;
 	}
-	
+
 	public abstract void changeTyre();
 }
