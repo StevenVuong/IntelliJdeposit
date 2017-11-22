f4221313ae1d5a9a729909249059c13e5dcceea9
StevenVuong <32569769+StevenVuong@users.noreply.github.com>
2017-11-22 21:56:59 +0000

    completed class. Ready to submit

---
+++ PHAS3459/src/module6/QuadraticTheory.java
@@ -0,0 +1,31 @@
+package module6;
+
+public abstract class QuadraticTheory implements Theory {
+	// class holding mathematical relation between x and y values (quadratic
+	// relationship)
+
+	// define member variables (a and b are the coefficients and c is a constant)
+	double x;
+	double y;
+	double a;
+	double b;
+	double c;
+
+	// constructor for QuadraticTheory object, setting parameters of function
+	public QuadraticTheory(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	// returns Y = ax^2 + bx + c
+	public double y(double x) {
+		return ((a * Math.pow(x, 2)) + (b * x) + c);
+	}
+
+	// toString method stating the function
+	public String toString() {
+		return (a+ " * x^2 + " + b + " * x + " + c);
+	}
+
+}
