2162fc914cd0f6c53a0f25243183cfe0fe643f39
Steven <steven_vuong@hotmail.co.uk>
2017-12-07 18:15:29 +0000

    has constructor with time object and void run() method

---
+++ PHAS3459/src/module8/CountdownTask.java
@@ -0,0 +1,41 @@
+package module8;
+
+/**
+ * Class to count down from an inputted number of seconds
+ * Implements the runnable interface
+ * 
+ * @author Steven Vuong
+ * @version: 07-12-2017
+ */
+
+
+public class CountdownTask implements Runnable{
+	
+	int time;
+	
+	/** 
+	 * Constructs a countdown object for a given number of seconds
+	 * @param seconds Number of seconds to count down
+	 */
+	public CountdownTask(int seconds) {
+		this.time = seconds;
+	}
+	
+	/**
+	 * Counts down from given seconds in object when called
+	 */
+	public void run() {
+		
+		while (time >= 1) { 
+			try {
+				Thread.sleep(1000); //Pause for 1 second for given times until t=0
+			}
+			catch (InterruptedException e) { //Stop early if main thread is interrupted (or runs forever)
+				System.out.println(e);
+			}
+		}
+		time --; //Reduce the timer by 1 second
+	}
+	
+
+}
