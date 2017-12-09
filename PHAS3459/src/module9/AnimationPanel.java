a6972db4e120655ffa92bf51f8572ab20f04ef51
Steven <steven_vuong@hotmail.co.uk>
2017-12-09 21:23:37 +0000

    adding vaporising of comet as it approaches sun

--- PHAS3459/src/module9/AnimationPanel.java
+++ PHAS3459/src/module9/AnimationPanel.java
@@ -68,6 +68,39 @@ public class AnimationPanel extends JPanel implements ActionListener {
 	
 		g.setColor(Color.WHITE);
 		g.fillOval((int)(Venus.x/2E10), (int)(Venus.y/2E10), Venus.radius, Venus.radius);
+	
+		g.setColor(Color.CYAN);
+		g.fillOval((int)(Earth.x/2E10), (int)(Earth.y/2E10), Earth.radius, Earth.radius);
+	
+		g.setColor(Color.RED);
+		g.fillOval((int)(Mars.x/2E10), (int)(Mars.y/2E10), Mars.radius, Mars.radius);
+
+		g.setColor(Color.PINK);
+		g.fillOval((int)(Jupiter.x/2E10), (int)(Jupiter.y/2E10), Jupiter.radius, Jupiter.radius);
+	
+		g.setColor(Color.ORANGE);
+		g.fillOval((int)(Saturn.x/2E10), (int)(Saturn.y/2E10), Saturn.radius, Saturn.radius);
+		
+		g.setColor(Color.CYAN);
+		g.fillOval((int)(Uranus.x/2E10), (int)(Uranus.y/2E10), Uranus.radius, Uranus.radius);
+		
+		g.setColor(Color.BLUE);
+		g.fillOval((int)(Neptune.x/2E10), (int)(Neptune.y/2E10), Neptune.radius, Neptune.radius);
+
+		g.setColor(Color.GREEN);
+		g.fillOval((int)(Icarus.x/2E10), (int)(Icarus.y/2E10), Icarus.radius, Icarus.radius);
+	
+		if (Comet.radius > 0) { //Do this if comet has not been completely vaporised
+			g.drawString("The comet has not yet been completely vaporised. ", -250, -230);
+			g.setColor(Color.MAGENTA);
+			g.fillOval((int)(Comet.x/2E10), (int)(Comet.y/2E10), Comet.radius, Comet.radius);
+			if (day % 50 == 0 && Math.sqrt(Comet.x*Comet.x + Comet.y*Comet.y) < 2E11) {
+				Comet.radius--; //reduces comet size as it approaches sun
+			}
+		}
+		else g.drawString("The comet has fully vaporised. ", -250, -230);
+		
+		g.drawString(("Time elapsed: " + day + " days"), -250 , -250);
 	}
 	
 }
